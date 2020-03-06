23b87db907882a98134e231dd4e099e5e5271324
EviIoannou <evievi@outlook.com>
2020-03-06 14:23:18 +0100

    all products in link in browse products

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,8 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/products/all">Our products</router-link> |
-      <router-link to="/products/">Our products</router-link> |
+      <router-link to="/products/">Browse products</router-link> |  <!-- test undefined parameter -->
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
