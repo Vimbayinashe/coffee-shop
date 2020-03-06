3346912525bb79a98d6399233a4b600e5f4ebdd9
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-03-06 14:52:29 +0100

    show stock after merge

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const sqlite = require('sqlite')
 
 const app = express()
-const cors = require('cors') 
+const cors = require('cors')
 app.use(cors())
 app.use('/images', express.static('images'))
 // app.use(express.json())
@@ -13,13 +13,20 @@ sqlite.open('productList.sqlite').then(database_ => {
   allProducts = database_
 })
 
+
+app.get('/products', (request, response) => {
+  allProducts.all('SELECT * FROM products').then((products) => {
+    response.send(products)
+  })
+})
+
 app.get('/products/:category', (request, response) => {
-  if(request.params.category=='all'){
-     allProducts.all('SELECT * FROM products').then((products) => {
+  if (request.params.category == 'all') {
+    allProducts.all('SELECT * FROM products').then((products) => {
       response.send(products)
-      })
-  }
     })
+  }
+})
 
 
 app.listen(3000)
