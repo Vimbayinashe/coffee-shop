a6fb544b87f473f024334d01b2b3347c5cbecb31
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-03-10 02:19:16 +0100

    Improve single product page

--- frontend/src/components/OneProduct.vue
+++ frontend/src/components/OneProduct.vue
@@ -1,6 +1,36 @@
 <template>
-  <div>
+  <div v-if="products">
     {{ $route.params.id}}
+    <h3> {{ products[$route.params.id].name}} </h3>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Brand</th>
+          <th>Type</th>
+          <th>Size</th>
+          <th>Price</th>
+          <th>Quantity</th>
+        </tr>
+      </thead>
+
+      <tbody>
+      <tr>
+        <td>{{ products[$route.params.id].name }}</td>
+        <td>{{ products[$route.params.id].brand }}</td>
+        <td>{{ products[$route.params.id].type }}</td>
+        <td>
+          {{ products[$route.params.id].weight }} 
+          {{ products[$route.params.id].unit }} 
+        </td>
+        <td>{{ products[$route.params.id].price }}</td>
+        <td>{{ products[$route.params.id].quantity }}</td>
+        <td>Edit</td>
+      </tr>
+      </tbody>
+    </table>
+
   </div>
 </template>
 
@@ -18,8 +48,14 @@ export default {
   },
   data () {
     return {
+      // index: $route.params.id,
       products: null
     }
+  },
+  methods: {
+    getIndex () {
+      this.index = this.products.findIndex(product => product.id === this.$route.params.id)
+    },
   },
   name: "OneProduct"
 };
