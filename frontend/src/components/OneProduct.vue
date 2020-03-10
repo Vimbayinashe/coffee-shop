886dea0d510f502fcd2932b89dc5bf4cb8d8d84c
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-03-10 13:45:14 +0100

    Add Edit stock details to All Products component

--- frontend/src/components/OneProduct.vue
+++ frontend/src/components/OneProduct.vue
@@ -26,11 +26,40 @@
         </td>
         <td>{{ products[$route.params.id].price }}</td>
         <td>{{ products[$route.params.id].quantity }}</td>
-        <td>Edit</td>
+        <!-- <td>Edit</td> -->
       </tr>
       </tbody>
     </table>
+    <button id="back-button">
+      <router-link to="/control-panel/">
+        Back
+      </router-link>
+    </button>
+    <button id="edit-button">Edit</button>
 
+    <div>
+      <div id="new-details">
+        <label for="price">Price:</label>
+        <input type="number" name="price" step="0.01" v-model="price">
+        <p>e.g. 2.99</p>
+        <label for="quantity">Quantity:</label>
+        <input type="number" name="quantity" step="1" v-model="quantity">
+      </div>
+
+      <p>Confirm product details</p>
+      <div id="confirm-details">
+        <div id="old-details" class="details">
+          <p>Old Details:</p>
+          <div>Price: {{ products[$route.params.id].price }} </div>
+          <div>Quantity {{ products[$route.params.id].quantity }} </div>
+        </div>
+        <div id="confirm-new" class="details">
+          <p>New Details:</p>
+          <div>Price: {{price}} </div>
+          <div>Quantity: {{quantity}} </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -48,8 +77,9 @@ export default {
   },
   data () {
     return {
-      // index: $route.params.id,
-      products: null
+      price: 0,
+      products: null,
+      quantity: 0
     }
   },
   methods: {
@@ -60,3 +90,37 @@ export default {
   name: "OneProduct"
 };
 </script>
+
+<style scoped>
+
+#confirm-details {
+  display: flex;
+  flex-direction: row;
+  /* font-size: 120%; */
+  margin: 1em 20vw;
+}
+
+.details {
+  margin: 1em;
+}
+
+#new-details {
+  display: flex;
+  flex-direction: row;
+  margin: 1em 20vw;
+  height: 1.2em;
+}
+
+#confirm-new {
+  background-color: lightcyan;
+}
+/* #old-details {
+  color: rgb(100, 95, 95);
+  /* font-size: 90%; */
+  /* font-style: italic; */
+/* } */ 
+
+p {
+  margin: 0 2em 0 0.5em;
+}
+</style>
